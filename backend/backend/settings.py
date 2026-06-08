c86484bde43a3d185f0de24425866b3a38d2114c
Chethanj14 <nnm23is504@nmamit.in>
2026-06-08 19:07:29 +0530

    Add WhiteNoise support

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,11 +40,12 @@ INSTALLED_APPS = [
 ]
 
 # MIDDLEWARE
-
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,7 +128,9 @@ USE_TZ = True
 
 # STATIC FILES
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
